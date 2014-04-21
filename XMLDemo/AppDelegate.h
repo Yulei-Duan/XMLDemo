8047989ee52eb7fa67e09eed75b841e3380dd534
Levi <suiyun@me.com>
2014-04-21 15:16:44 +0800

    Initial Commit

---
+++ XMLDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XMLDemo
+//
+//  Created by YuLei on 14-4-21.
+//  Copyright (c) 2014年 ___DuanYuLei___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XMLDemoTests/XMLDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  XMLDemoTests.m
+//  XMLDemoTests
+//
+//  Created by YuLei on 14-4-21.
+//  Copyright (c) 2014年 ___DuanYuLei___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XMLDemoTests : XCTestCase
+
+@end
+
+@implementation XMLDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
